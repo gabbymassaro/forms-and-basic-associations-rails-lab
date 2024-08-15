7976f5f5219e444aabdb68f45d38865ef4301f7f
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-15 13:46:44 -0400

    New Song Form Renders Correctly

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,11 @@
 class Genre < ActiveRecord::Base
-  # add associations
+  has_many :songs
+
+  def genre_id_change=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_id_change
+     self.genre ? self.genre.name : nil
+  end
 end
